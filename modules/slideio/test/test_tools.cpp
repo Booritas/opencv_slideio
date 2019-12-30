90c41947f19af5ab34a4750b8df53153b7d5fe14
Booritas <stanislav.melnikov@gmail.com>
2019-12-30 20:00:36 +0100

    fix data path in unit tests

--- modules/slideio/test/test_tools.cpp
+++ modules/slideio/test/test_tools.cpp
@@ -1,33 +1,24 @@
 #include "test_precomp.hpp"
 #include "testtools.hpp"
 #include <boost/dll/runtime_symbol_info.hpp>
+#include "opencv2/ts.hpp"
 
 namespace opencv_test {
 
 std::string TestTools::getTestImageDirectory()
 {
-	boost::filesystem::path modulePath = boost::dll::program_location();
-	boost::filesystem::path testDirPath;
-	for(const auto& item : modulePath)
-	{
-		if(item.string().compare("bin")==0){
-			testDirPath /= "TestImages";
-			break;
-		}
-		else{
-			testDirPath /= item;
-		}
-	}
-	return testDirPath.string();
+    boost::filesystem::path testDirPath = cvtest::TS::ptr()->get_data_path();
+    testDirPath /= "slideio";
+    return testDirPath.string();
 }
 
 std::string TestTools::getTestImagePath(const std::string& subfolder, const std::string& image)
 {
-	boost::filesystem::path imagePath(getTestImageDirectory());
-	if(!subfolder.empty())
-		imagePath /= subfolder;
-	imagePath /= image;
-	return imagePath.string();
+    boost::filesystem::path imagePath(getTestImageDirectory());
+    if(!subfolder.empty())
+        imagePath /= subfolder;
+    imagePath /= image;
+    return imagePath.string();
 }
 
 }
